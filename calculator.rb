779aa90f1470dd5444ec5144a2d90692974ac118
patrick leal <pjleal3846@icloud.com>
2020-01-11 10:58:45 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,11 +2,6 @@ first_number = 1.2
 second_number = 4
 sum = first_number+second_number
 difference = first_number - second_number
-
+poduct = first_number*second_number
 # Add your variables here
-def calculator()
-  expect( first_number).to be_an(Integer).or be_a(Float)
-expect(second_number).to be_an(Integer).or be_a(Float) 
-  expect(second_number).not_to equal(0)
-  expect(sum).to eq(first_number+second_number)
-end
+
