c2b319879aa10cde08a4564436c307beb219a3be
patrick leal <pjleal3846@icloud.com>
2020-01-11 10:59:44 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,6 +3,6 @@ second_number = 4
 sum = first_number+second_number
 difference = first_number - second_number
 poduct = first_number*second_number
-
+quotent = first_number/second_number
 # Add your variables here
 
