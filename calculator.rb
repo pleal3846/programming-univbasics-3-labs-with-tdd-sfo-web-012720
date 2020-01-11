78cbe77083064a3db8990e787fd525a81af48cd7
patrick leal <pjleal3846@icloud.com>
2020-01-11 10:53:31 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,8 +3,6 @@ second_number = 4
 # Add your variables here
 def calculator()
   expect( first_number).to be_an(Integer).or be_a(Float)
-end
-it 
 expect(second_number).to be_an(Integer).or be_a(Float) 
   expect(second_number).not_to equal(0)
 end
