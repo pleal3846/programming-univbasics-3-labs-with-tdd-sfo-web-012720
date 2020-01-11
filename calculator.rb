38087f2b60518f84c0aade67a130baa87717fc3f
patrick leal <pjleal3846@icloud.com>
2020-01-11 11:01:27 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,8 @@
+first_number = 1.2
+second_number = 4
+sum = first_number+second_number
+difference = first_number - second_number
+product = first_number*second_number
+quotient = first_number/second_number
 # Add your variables here
+
