ac30b80e2a17711ff963e917cf9b87c9b621e4c2
patrick leal <pjleal3846@icloud.com>
2020-01-11 10:51:51 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,6 +4,6 @@ second_number = 4
 def calculator()
   expect( first_number).to be_an(Integer).or be_a(Float)
 end
-do expect(second_number).to be_an(Integer).or be_a(Float) 
+expect(second_number).to be_an(Integer).or be_a(Float) 
   expect(second_number).not_to equal(0)
 end
