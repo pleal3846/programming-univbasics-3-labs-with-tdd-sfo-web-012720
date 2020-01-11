ef96ee891bacd3a4e53e696cdbadc65ce8187691
patrick leal <pjleal3846@icloud.com>
2020-01-11 10:55:28 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -5,5 +5,5 @@ def calculator()
   expect( first_number).to be_an(Integer).or be_a(Float)
 expect(second_number).to be_an(Integer).or be_a(Float) 
   expect(second_number).not_to equal(0)
-  expect(sum).to eq(first_number + second_number
+  expect(sum).to eq(first_number + second_number)
 end
