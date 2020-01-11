03a4ebbc32412ae2e1c141aacfbccbe4e9cf5a84
patrick leal <pjleal3846@icloud.com>
2020-01-11 11:00:30 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,7 +2,7 @@ first_number = 1.2
 second_number = 4
 sum = first_number+second_number
 difference = first_number - second_number
-poduct = first_number*second_number
+product = first_number*second_number
 quotent = first_number/second_number
 # Add your variables here
 
