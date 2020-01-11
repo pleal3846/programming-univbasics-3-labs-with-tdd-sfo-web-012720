c4a947f906a47a538df832049f98e8222039d3e4
patrick leal <pjleal3846@icloud.com>
2020-01-11 10:57:18 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,7 @@
 first_number = 1.2
 second_number = 4
 sum = first_number+second_number
+difference = 
 # Add your variables here
 def calculator()
   expect( first_number).to be_an(Integer).or be_a(Float)
